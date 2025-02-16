9b51685e19825bda75f8d1b662f870b3866cd621
Maheen Ansari <muhammadyaseen8708@gmail.com>
2025-02-16 09:58:21 +0500

    Update Dashboard.jsx

--- LMS/src/Components/Dashboard/Dashboard.jsx
+++ LMS/src/Components/Dashboard/Dashboard.jsx
@@ -14,9 +14,9 @@ import ListItemText from '@mui/material/ListItemText';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ExpandLessIcon from '@mui/icons-material/ExpandLess';
 import { Route, Routes, useNavigate } from 'react-router-dom';
-import EmailIcon from '@mui/icons-material/Email';
 
 import StudentRegistration from './StudentRegistration';
+import StudentList from './StudentList'
 import TeacherRegistration from './TeacherRegistration';
 import TeacherList from './TeacherList';
 import SubjectAdd from './SubjectAdd';
@@ -28,66 +28,155 @@ import ClassForm from './ClassForm';
 import FeeStructure from './FeeStructure';
 import FeeVoucher from './FeeVoucher';
 import FeeSubmission from './FeeSubmission';
-import StudentList from './StudentList';
+import EmailIcon from '@mui/icons-material/Email';
 import Admission from './Admission';
 import ExamSchedule from './ExamSchedule';
 import ExamResult from './ExamResult';
+import LogoutButton from './LogoutButton';
 
 const drawerWidth = 240;
 
 const pages = [
   {
     name: 'Students',
+    route: 'Student',
     children: [
-      { name: 'StudentRegistration', route: 'studentRegistration' },
-      { name: 'StudentList', route: 'studentList' },
+      {
+        name: 'StudentRegistration',
+        route: 'studentRegistration',
+      },
+      {
+        name: 'StudentList',
+        route: 'studentList',
+      },
     ],
   },
   {
     name: 'Teachers',
+    route: 'Teacher',
     children: [
-      { name: 'TeacherRegistration', route: 'teacherRegistration' },
-      { name: 'TeacherList', route: 'teacherList' },
+      {
+        name: 'TeacherRegistration',
+        route: 'teacherRegistration',
+      },
+      {
+        name: 'TeacherList',
+        route: 'teacherList',
+      },
     ],
   },
   {
     name: 'Subjects',
+    route: 'Subjects',
     children: [
-      { name: 'SubjectAdd', route: 'subjectAdd' },
-      { name: 'SubjectList', route: 'subjectList' },
+      {
+        name: 'SubjectAdd',
+        route: 'subjectAdd',
+      },
+      {
+        name: 'SubjectList',
+        route: 'subjectList',
+      },
     ],
   },
   {
     name: 'Syllabus',
+    route: 'Syllabus',
     children: [
-      { name: 'SyllabusForm', route: 'SyllabusForm' },
-      { name: 'SyllabusList', route: 'SyllabusList' },
+      {
+        name: 'SyllabusForm',
+        route: 'SyllabusForm',
+      },
+      {
+        name: 'SyllabusList',
+        route: 'SyllabusList',
+      },
+    ],
+  },
+  {
+    name: 'School',
+    route: 'School',
+    children: [
+      {
+        name: 'StudentRegistration',
+        route: 'studentRegistration',
+      },
+      {
+        name: 'TeacherRegistration',
+        route: 'teacherRegistration',
+      },
+    ],
+  },
+  {
+    name: 'Class',
+    route: 'Class',
+    children: [
+      {
+        name: 'classForm',
+        route: 'classForm',
+      },
+      {
+        name: 'Classlist',
+        route: 'classlist',
+      },
     ],
   },
   {
     name: 'Fees',
+    route: 'Fees',
     children: [
-      { name: 'FeeStructure', route: 'feestructure' },
-      { name: 'FeeVoucher', route: 'feevoucher' },
-      { name: 'FeeSubmission', route: 'feesubmission' },
+      {
+        name: 'FeeStructure',
+        route: 'feestructure',
+      },
+      {
+        name: 'FeeVoucher',
+        route: 'feevoucher',
+      },
+      {
+        name: 'FeeSubmission',
+        route: 'feesubmission',
+      },
     ],
   },
   {
     name: 'Admission',
-    children: [{ name: 'Admission Form', route: 'admission' }],
+    route: 'Admission',
+    children: [
+      {
+        name: 'Admission Form',
+        route: 'admission',
+      },
+    ],
   },
   {
     name: 'Exam',
+    route: 'exam',
     children: [
-      { name: 'ExamSchedule', route: 'examSchedule' },
-      { name: 'ExamResult', route: 'examresult' },
+      {
+        name: 'ExamSchedule',
+        route: 'examSchedule',
+      },
+      {
+        name: 'ExamResult',
+        route: 'examresult',
+      },
     ],
   },
 ];
 
 export default function Dashboard() {
   const navigate = useNavigate();
-  const [openDropdown, setOpenDropdown] = React.useState({});
+  const [openDropdown, setOpenDropdown] = React.useState({
+    students: false,
+    teachers: false,
+    subjects: false,
+    syllabus: false,
+    school: false,
+    class: false,
+    fees: false,
+    admission:false,
+  });
 
   const handleDropdownToggle = (section) => {
     setOpenDropdown((prevState) => ({
@@ -99,11 +188,18 @@ export default function Dashboard() {
   return (
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
-      <AppBar position="fixed" sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px`, backgroundColor: '#388E3C' }}>
-        <Toolbar>
-          <Typography variant="h6" noWrap>
+     
+      <AppBar
+        position="fixed"
+        sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
+      >
+        <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Typography variant="h6" noWrap component="div">
             <h5>LEARNING MANAGEMENT SYSTEM</h5>
           </Typography>
+          <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+            <LogoutButton />
+          </Box>
         </Toolbar>
       </AppBar>
 
@@ -124,13 +220,23 @@ export default function Dashboard() {
         <List>
           {pages.map((obj, index) => (
             <React.Fragment key={index}>
-              <ListItemButton onClick={() => handleDropdownToggle(obj.name.toLowerCase())} sx={{ display: 'flex', justifyContent: 'space-between' }}>
+              <ListItemButton
+                onClick={() => handleDropdownToggle(obj.name.toLowerCase())}
+                sx={{ display: 'flex', justifyContent: 'space-between' }}
+              >
                 <ListItemIcon sx={{ minWidth: 'auto' }}>
-                  <EmailIcon sx={{ color: '#388E3C' }} />
+                  <EmailIcon sx={{ color: '#388E3C' }} /> {/* Green color */}
                 </ListItemIcon>
-                <ListItemText primary={obj.name} sx={{ fontWeight: 'bold', color: '#388E3C' }} />
+                <ListItemText
+                  primary={obj.name}
+                  sx={{ fontWeight: 'bold', color: '#388E3C' }} // Green color
+                />
                 <ListItemIcon sx={{ minWidth: 'auto' }}>
-                  {openDropdown[obj.name.toLowerCase()] ? <ExpandLessIcon sx={{ color: '#388E3C' }} /> : <ExpandMoreIcon sx={{ color: '#388E3C' }} />}
+                  {openDropdown[obj.name.toLowerCase()] ? (
+                    <ExpandLessIcon />
+                  ) : (
+                    <ExpandMoreIcon />
+                  )}
                 </ListItemIcon>
               </ListItemButton>
 
@@ -138,7 +244,10 @@ export default function Dashboard() {
                 <div>
                   {obj.children.map((child, childIndex) => (
                     <ListItem key={childIndex} disablePadding>
-                      <ListItemButton sx={{ pl: 4 }} onClick={() => navigate(`/dashboard/${child.route}`)}>
+                      <ListItemButton
+                        sx={{ pl: 4 }}
+                        onClick={() => navigate(`/dashboard/${child.route}`)}
+                      >
                         <ListItemText primary={child.name} />
                       </ListItemButton>
                     </ListItem>
@@ -151,7 +260,10 @@ export default function Dashboard() {
         <Divider />
       </Drawer>
 
-      <Box component="main" sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3, ml: `${drawerWidth}px` }}>
+      <Box
+        component="main"
+        sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3, ml: `${drawerWidth}px` }}
+      >
         <Routes>
           <Route path="studentRegistration" element={<StudentRegistration />} />
           <Route path="studentList" element={<StudentList />} />
@@ -166,9 +278,9 @@ export default function Dashboard() {
           <Route path="feestructure" element={<FeeStructure />} />
           <Route path="feevoucher" element={<FeeVoucher />} />
           <Route path="feesubmission" element={<FeeSubmission />} />
-          <Route path="admission" element={<Admission />} />
-          <Route path="examSchedule" element={<ExamSchedule />} />
-          <Route path="examresult" element={<ExamResult />} />
+          <Route path="admission" element={< Admission />} />
+          <Route path="examSchedule" element={< ExamSchedule />} />
+          <Route path="examresult" element={< ExamResult />} />
         </Routes>
       </Box>
     </Box>
